d8274c59f51b4d7bfb87f141bef6a186f945b080
DavidAarsen <davidaarsen@gmail.com>
2014-10-25 18:18:22 -0700

    commit

--- headers/constants.h
+++ headers/constants.h
@@ -1,10 +1,15 @@
-
+//--If an Array us used to store square information this will be the sumbers --//
 const int a8 = 0,  b8 = 1,  c8 = 2,  d8 = 3,  e8 = 4,  f8 = 5,  g8 = 6,  h8 = 7;
 const int a7 = 8,  b7 = 9,  c7 = 10, d7 = 11, e7 = 12, f7 = 13, g7 = 14, h7 = 15;
 const int a6 = 16, b6 = 17, c6 = 18, d6 = 19, e6 = 20, f6 = 21, g6 = 22, h6 = 23;
 const int a5 = 24, b5 = 25, c5 = 26, d5 = 27, e5 = 28, f5 = 29, g5 = 30, h5 = 31;
-const int a4 = 0, b4 = 0, c4 = 0, d4 = 0, e4 = 0, f4 = 0, g4 = 0, h4 = 0;
-const int a3 = 0, b3 = 0, c3 = 0, d3 = 0, e3 = 0, f3 = 0, g3 = 0, h3 = 0;
-const int a2 = 0, b2 = 0, c2 = 0, d2 = 0, e2 = 0, f2 = 0, g2 = 0, h2 = 0;
-const int a1 = 0, b1 = 0, c1 = 0, d1 = 0, e1 = 0, f1 = 0, g1 = 0, h1 = 0;
-          
+const int a4 = 32, b4 = 33, c4 = 34, d4 = 35, e4 = 36, f4 = 37, g4 = 38, h4 = 39;
+const int a3 = 40, b3 = 41, c3 = 42, d3 = 43, e3 = 44, f3 = 45, g3 = 46, h3 = 47;
+const int a2 = 48, b2 = 49, c2 = 50, d2 = 51, e2 = 52, f2 = 53, g2 = 54, h2 = 55;
+const int a1 = 56, b1 = 57, c1 = 58, d1 = 59, e1 = 60, f1 = 61, g1 = 62, h1 = 63;
+/*
+a8 b8 c8 d8 e8 f8 g8 h8
+a7 b7 c7 d7 e7 f7 g7 h7
+a6 b6 c6 d6 e6 f6 g6 h6
+ect.....
+*/
